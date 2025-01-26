9ff2064d209e3cec8ee8fca4ff2c77bbc8ec3f49
Aled Cuda <aled@ucsb.edu>
2025-01-26 11:33:22 -0800

    Rough prototype

--- src/app.rs
+++ src/app.rs
@@ -1,44 +1,91 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use std::io::Read;
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+use colorous::VIRIDIS;
+use egui_plot::{Line, Plot, Points, VLine};
+use ndarray::prelude::*;
+use ndarray_npy::NpzReader;
+
+use egui::{Color32, Id};
+use num_complex::ComplexFloat;
+use std::collections::HashMap;
+
+use crate::{PowerSweepConfig, PowerSweepValues};
+
+pub struct ClickThrough {
+    gamma: f64,
+    psweep: PowerSweepConfig,
+    resonator: usize,
+    settings: HashMap<usize, BiasSetting>,
+    values: PowerSweepValues,
+    freq_range: (f64, f64),
+    freq_max: (f64, f64),
+    atten_range: (f64, f64),
+    atten_max: (f64, f64),
+    show_mag: bool,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
-        Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
-        }
-    }
-}
-
-impl TemplateApp {
+impl ClickThrough {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
+        let mut json = String::new();
+        std::fs::File::open("./psweepconfig.json")
+            .unwrap()
+            .read_to_string(&mut json)
+            .unwrap();
+        let mut pv = NpzReader::new(std::fs::File::open("./psweep.npz").unwrap()).unwrap();
+        let psweep: PowerSweepConfig = serde_json::from_str(&json).unwrap();
+        let maxo: f64 = psweep.attens.iter().fold(f64::MIN, |a, b| a.max(b.0));
+        let mino: f64 = psweep.attens.iter().fold(f64::MAX, |a, b| a.min(b.0));
+        let maxf: f64 = psweep
+            .sweep_config
+            .steps
+            .iter()
+            .fold(f64::MIN, |a, b| a.max(*b));
+        let minf: f64 = psweep
+            .sweep_config
+            .steps
+            .iter()
+            .fold(f64::MAX, |a, b| a.min(*b));
 
-        Default::default()
+        ClickThrough {
+            gamma: 1.0,
+            resonator: 0,
+            psweep,
+            values: PowerSweepValues::from_reader(&mut pv),
+            settings: HashMap::new(),
+            freq_range: (minf, maxf),
+            freq_max: (minf, maxf),
+            atten_range: (mino, maxo),
+            atten_max: (mino, maxo),
+            show_mag: true,
+        }
     }
 }
 
-impl eframe::App for TemplateApp {
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
+struct BiasPoint {
+    output_atten: usize,
+    freq: usize,
+}
+
+#[derive(Debug)]
+struct BiasSetting {
+    output_atten: f64,
+    freq: f64,
+}
+
+impl eframe::App for ClickThrough {
     /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+    fn save(&mut self, _storage: &mut dyn eframe::Storage) {
+        // eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
@@ -66,44 +113,176 @@ impl eframe::App for TemplateApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
+                ui.spacing_mut().slider_width = ui.available_width() / 3.;
+                ui.add(
+                    egui::Slider::new(&mut self.freq_range.0, self.freq_max.0..=self.freq_range.1)
+                        .clamping(egui::SliderClamping::Always),
+                );
+                ui.add(
+                    egui::Slider::new(&mut self.freq_range.1, self.freq_range.0..=self.freq_max.1)
+                        .clamping(egui::SliderClamping::Always),
+                );
+                ui.label("Frequency Range");
+            });
+            ui.horizontal(|ui| {
+                ui.spacing_mut().slider_width = ui.available_width() / 3.;
+                ui.add(
+                    egui::Slider::new(
+                        &mut self.atten_range.0,
+                        self.atten_max.0..=self.atten_range.1,
+                    )
+                    .clamping(egui::SliderClamping::Always),
+                );
+                ui.add(
+                    egui::Slider::new(
+                        &mut self.atten_range.1,
+                        self.atten_range.0..=self.atten_max.1,
+                    )
+                    .clamping(egui::SliderClamping::Always),
+                );
+                ui.label("Attenuation Range");
+            });
+            ui.horizontal(|ui| {
+                ui.add(
+                    egui::Slider::new(&mut self.resonator, 0..=self.values.iq[0].1.shape()[0] - 1)
+                        .clamping(egui::SliderClamping::Always)
+                        .text("Resonator"),
+                );
+                ui.add(egui::Separator::default());
+                ui.add(egui::Slider::new(&mut self.gamma, 0.0..=3.0).text("Gamma"));
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            let h = ui.available_height();
+            ui.horizontal(|ui| {
+                ui.set_height(h);
+                let iqs: Vec<(usize, f64, Vec<[f64; 2]>, Vec<[f64; 2]>)> = self
+                    .values
+                    .iq
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, ((o, _), _))| *o >= self.atten_range.0 && *o <= self.atten_range.1)
+                    .map(|(ai, ((o, i), iq))| {
+                        let gain = (10f64.powf((*i + *o * self.gamma) / 10.)).sqrt();
+                        (
+                            ai,
+                            *o,
+                            iq.slice(s![self.resonator, ..])
+                                .iter()
+                                .enumerate()
+                                .filter(|(f, _)| {
+                                    let f = self.psweep.sweep_config.steps[*f];
+                                    f >= self.freq_range.0 && f <= self.freq_range.1
+                                })
+                                .map(|(_, v)| [v.re as f64 * gain, v.im as f64 * gain])
+                                .collect(),
+                            iq.slice(s![self.resonator, ..])
+                                .iter()
+                                .enumerate()
+                                .filter(|(f, _)| {
+                                    let f = self.psweep.sweep_config.steps[*f];
+                                    f >= self.freq_range.0 && f <= self.freq_range.1
+                                })
+                                .map(|(f, c)| {
+                                    [self.psweep.sweep_config.steps[f], gain * c.abs() as f64]
+                                })
+                                .collect(),
+                        )
+                    })
+                    .collect();
 
-            ui.separator();
+                let fmap: Vec<usize> = self
+                    .psweep
+                    .sweep_config
+                    .steps
+                    .iter()
+                    .enumerate()
+                    .filter(|(fi, f)| **f >= self.freq_range.0 && **f <= self.freq_range.1)
+                    .map(|(fi, _)| fi)
+                    .collect();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+                let mut ids: HashMap<Id, BiasPoint> =
+                    HashMap::with_capacity(self.values.iq.len() * 1024);
+
+                let pr = Plot::new(format!("Clickey{}{}", self.resonator, self.gamma))
+                    .show_axes([false, false])
+                    .width(ui.available_width() / 2.)
+                    .data_aspect(1.0)
+                    .auto_bounds([true, true].into())
+                    .show(ui, |plotui| {
+                        iqs.iter().for_each(|(ai, o, l, _)| {
+                            let t = (*o - self.atten_range.0)
+                                / (self.atten_range.1 - self.atten_range.0);
+                            let t = 1. - t;
+                            let color = VIRIDIS.eval_continuous(t);
+                            let color = Color32::from_rgb(color.r, color.g, color.b);
+                            for (num, point) in l.iter().enumerate() {
+                                let bp = BiasPoint {
+                                    output_atten: *ai,
+                                    freq: fmap[num],
+                                };
+                                let id = Id::new(bp);
+                                ids.insert(id, bp);
+                                plotui.points(
+                                    Points::new(vec![*point]).color(color).radius(4.).id(id),
+                                )
+                            }
+                            plotui.line(Line::new(l.clone()).color(color).allow_hover(false))
+                        })
+                    });
+
+                let mut bp = None;
+                if let Some(h) = pr.hovered_plot_item {
+                    bp = ids.get(&h);
+                }
+
+                Plot::new("Showey")
+                    .show_axes([true, true])
+                    .width(ui.available_width())
+                    .allow_zoom(false)
+                    .allow_scroll(false)
+                    .allow_drag(false)
+                    .show(ui, |plotui| {
+                        if self.show_mag {
+                            iqs.iter().for_each(|(_, o, _, l)| {
+                                let t = (*o - self.atten_range.0)
+                                    / (self.atten_range.1 - self.atten_range.0);
+                                let t = 1. - t;
+                                let color = VIRIDIS.eval_continuous(t);
+                                let mut color = Color32::from_rgb(color.r, color.g, color.b);
+                                if bp.is_some() {
+                                    color = color.gamma_multiply(0.1);
+                                }
+                                plotui.line(Line::new(l.clone()).color(color).allow_hover(false))
+                            });
+                        }
+                        if let Some(bp) = bp {
+                            plotui.vline(VLine::new(self.psweep.sweep_config.steps[bp.freq]));
+                            for (ai, _, _, v) in iqs.iter() {
+                                if *ai == bp.output_atten {
+                                    plotui.line(Line::new(v.clone()).highlight(self.show_mag));
+                                    break;
+                                }
+                            }
+                            plotui.set_auto_bounds([true, true].into());
+                        }
+                    });
+
+                if pr.response.clicked() {
+                    if let Some(bp) = bp {
+                        let bs = BiasSetting {
+                            output_atten: self.psweep.attens[bp.output_atten].0,
+                            freq: self.psweep.sweep_config.steps[bp.freq],
+                        };
+                        self.settings.insert(self.resonator, bs);
+                        self.resonator += 1;
+                    }
+                }
+            });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,155 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
-pub use app::TemplateApp;
+pub use app::ClickThrough;
+
+use serde::{Deserialize, Serialize};
+
+use ndarray::Array2;
+use num_complex::Complex;
+use std::collections::HashMap;
+
+use ndarray_npy::NpzReader;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Waveform {
+    freqs: Vec<f64>,
+    amps: Vec<f64>,
+    phases: Vec<f64>,
+    n_samples: u64,
+    _sample_rate: f64,
+    allow_sat: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SweepConfig {
+    steps: Vec<f64>,
+    waveform: Waveform,
+    lo_center: f64,
+    average: u64,
+    attens: Option<(f64, f64)>,
+    tap: String,
+    rmses: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PowerSweepConfig {
+    attens: Vec<(f64, f64)>,
+    sweep_config: SweepConfig,
+}
+
+pub struct PowerSweepValues {
+    iq: Vec<((f64, f64), Array2<Complex<f32>>)>,
+    iqs: Option<HashMap<(f64, f64), Array2<Complex<f32>>>>,
+}
+
+impl PowerSweepValues {
+    pub fn from_reader<T: std::io::Read + std::io::Seek>(
+        reader: &mut NpzReader<T>,
+    ) -> PowerSweepValues {
+        let mut psv = PowerSweepValues {
+            iq: Vec::new(),
+            iqs: None,
+        };
+
+        for file in reader.names().unwrap().iter() {
+            if &file[0..1] == "o" && &file[file.len() - 2..] == "iq" {
+                let parts: Vec<_> = file[1..file.len() - 2].split_terminator("d").collect();
+                assert_eq!(parts.len(), 2);
+                let a = (parts[0].parse().unwrap(), parts[1].parse().unwrap());
+                psv.iq.push((a, reader.by_name(file).unwrap()));
+            }
+        }
+
+        psv
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Read;
+
+    use super::*;
+
+    #[test]
+    fn deserialize_waveform() {
+        let blah = r#"
+        {
+            "freqs": [0.0, 0.1],
+            "amps": [0.0, 0.1],
+            "phases": [0.0, 0.1],
+            "n_samples": 10,
+            "_sample_rate": 0.1,
+            "allow_sat": false
+        }
+        "#;
+        let _wav: Waveform = serde_json::from_str(blah).unwrap();
+    }
+
+    #[test]
+    fn deserialize_sweep() {
+        let blah = r#"
+        {
+            "steps": [0.0, 0.1],
+            "waveform": {
+                "freqs": [0.0, 0.1],
+                "amps": [0.0, 0.1],
+                "phases": [0.0, 0.1],
+                "n_samples": 10,
+                "_sample_rate": 0.1,
+                "allow_sat": false
+            },
+            "lo_center": 6000.0,
+            "average": 10,
+            "attens": [0, 1],
+            "tap": "ddciq",
+            "rmses": true
+        }
+        "#;
+        let _conf: SweepConfig = serde_json::from_str(blah).unwrap();
+    }
+
+    #[test]
+    fn deserialize_psweep() {
+        let blah = r#"
+        {
+            "attens": [[0, 1], [1, 2]],
+            "sweep_config": {
+                "steps": [0.0, 0.1],
+                "waveform": {
+                    "freqs": [0.0, 0.1],
+                    "amps": [0.0, 0.1],
+                    "phases": [0.0, 0.1],
+                    "n_samples": 10,
+                    "_sample_rate": 0.1,
+                    "allow_sat": false
+                },
+                "lo_center": 6000.0,
+                "average": 10,
+                "attens": [0, 1],
+                "tap": "ddciq",
+                "rmses": true
+            }
+        }
+        "#;
+        let _conf: PowerSweepConfig = serde_json::from_str(blah).unwrap();
+    }
+
+    #[test]
+    fn deserialize_file() {
+        let mut blah = String::new();
+        std::fs::File::open("./psweepconfig.json")
+            .unwrap()
+            .read_to_string(&mut blah)
+            .unwrap();
+        let _conf: PowerSweepConfig = serde_json::from_str(&blah).unwrap();
+    }
+
+    #[test]
+    fn load_file() {
+        let sweep = PowerSweepValues::from_reader(
+            &mut NpzReader::new(std::fs::File::open("./psweep.npz").unwrap()).unwrap(),
+        );
+        assert!(sweep.iq.len() > 1);
+    }
+}
